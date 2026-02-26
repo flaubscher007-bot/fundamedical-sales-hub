40dc4cfd9377f4fee5e179945bb562fdd2358c19
Francisco Laubscher <flaubscher1984@gmail.com>
2026-02-26 11:09:50 +0000

    File changes

--- src/pages/BULDashboard.jsx
+++ src/pages/BULDashboard.jsx
@@ -1,17 +1,14 @@
 export default function BULDashboard() {
   return (
-    <div className="w-full h-full flex flex-col">
-      <div className="w-full flex-1" style={{ minHeight: "calc(100vh - 8rem)" }}>
-        <iframe
-          title="KAC Tracker"
-          width="100%"
-          height="100%"
-          src="https://app.powerbi.com/view?r=eyJrIjoiZDRlYzliNTMtNzc4My00ZmRmLThmYjktYmU2MDg4NGVjMzkzIiwidCI6ImViNzdjYzEwLTc5NDAtNDhjMy1hMDMzLWJkZjU3ODIzNDk0YiJ9&pageName=ebef07879a4406f00591"
-          frameBorder="0"
-          allowFullScreen={true}
-          style={{ minHeight: "calc(100vh - 8rem)" }}
-        />
-      </div>
+    <div className="fixed inset-0 top-16 flex flex-col">
+      <iframe
+        title="KAC Tracker"
+        width="100%"
+        height="100%"
+        src="https://app.powerbi.com/view?r=eyJrIjoiZDRlYzliNTMtNzc4My00ZmRmLThmYjktYmU2MDg4NGVjMzkzIiwidCI6ImViNzdjYzEwLTc5NDAtNDhjMy1hMDMzLWJkZjU3ODIzNDk0YiJ9&pageName=ebef07879a4406f00591"
+        frameBorder="0"
+        allowFullScreen={true}
+      />
     </div>
   );
 }
--- src/pages/FinanceDashboard.jsx
+++ src/pages/FinanceDashboard.jsx
@@ -1,17 +1,14 @@
 export default function FinanceDashboard() {
   return (
-    <div className="w-full h-full flex flex-col">
-      <div className="w-full flex-1" style={{ minHeight: "calc(100vh - 8rem)" }}>
-        <iframe
-          title="TEAM FINANCE STATS"
-          width="100%"
-          height="100%"
-          src="https://app.powerbi.com/view?r=eyJrIjoiZTljNDEyMTItNzQ4Ny00MTZmLTk0MmUtNDEyYjUwM2YxN2UxIiwidCI6ImViNzdjYzEwLTc5NDAtNDhjMy1hMDMzLWJkZjU3ODIzNDk0YiJ9&pageName=ReportSection9d871590762418041a6e"
-          frameBorder="0"
-          allowFullScreen={true}
-          style={{ minHeight: "calc(100vh - 8rem)" }}
-        />
-      </div>
+    <div className="fixed inset-0 top-16 flex flex-col">
+      <iframe
+        title="TEAM FINANCE STATS"
+        width="100%"
+        height="100%"
+        src="https://app.powerbi.com/view?r=eyJrIjoiZTljNDEyMTItNzQ4Ny00MTZmLTk0MmUtNDEyYjUwM2YxN2UxIiwidCI6ImViNzdjYzEwLTc5NDAtNDhjMy1hMDMzLWJkZjU3ODIzNDk0YiJ9&pageName=ReportSection9d871590762418041a6e"
+        frameBorder="0"
+        allowFullScreen={true}
+      />
     </div>
   );
 }
