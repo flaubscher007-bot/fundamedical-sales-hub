c1240b78f515f6d939e38b04f215a641d9651a33
Francisco Laubscher <flaubscher1984@gmail.com>
2026-02-27 09:28:08 +0000

    File changes

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -43,7 +43,7 @@ const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
 CardDescription.displayName = "CardDescription"
 
 const CardContent = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("p-6 pt-0 text-white", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
