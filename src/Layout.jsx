a87e3a9dfcc0eb967e6ec65238afb81b5f858af3
Francisco Laubscher <flaubscher1984@gmail.com>
2026-02-27 12:12:25 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -50,8 +50,6 @@ const expertItems = [
 ];
 
 const mainNavItems = [
-  { name: "Experts", icon: Stethoscope, page: "Experts" },
-  { name: "Import Experts", icon: Upload, page: "ExpertImportManager" },
   { name: "Appointment Tools", icon: Wrench, page: "AppointmentTools" },
   { name: "Contract Tools", icon: FileBadge, page: "Contracts" },
   { name: "Expense Tools", icon: Receipt, page: "ExpensesHub" },
