9ed755d02968d305e92459abb754a194a9f0af3d
Francisco Laubscher <flaubscher1984@gmail.com>
2026-02-27 09:38:16 +0000

    File changes

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground text-[#92F21D] border-[#34CCD0]",
+          "border border-input bg-[#008AC1] shadow-sm hover:bg-accent hover:text-accent-foreground text-[#92F21D] border-[#34CCD0]",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 text-white",
         ghost: "hover:bg-accent hover:text-accent-foreground text-[#92F21D]",
