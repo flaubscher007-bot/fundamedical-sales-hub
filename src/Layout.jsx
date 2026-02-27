61853db39b382b280c2cb6b887b0cffd3dd74981
Francisco Laubscher <flaubscher1984@gmail.com>
2026-02-27 08:30:19 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -46,7 +46,6 @@ const mainNavItems = [
   { name: "Follow-Up Rules", icon: FileText, page: "FollowUpRules" },
   { name: "Goals", icon: TrendingUp, page: "Goals" },
   { name: "Team Calendar", icon: LayoutDashboard, page: "TeamCalendar" },
-
   { name: "BUL Management", icon: TrendingUp, page: "BULManagement" },
   { name: "BUL Performance", icon: TrendingUp, page: "BULPerformance" },
   { name: "Company Targets", icon: TrendingUp, page: "CompanyTargets" },
